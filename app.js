96a6715fe26e5d67ef33e4584d23ce742fa8ed33
Riley Hays <haysrg@miamioh.edu>
2022-02-04 09:20:10 -0500

    initialized database and created two entries into the database

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'movies.db'
 });
+
+// Movie model
+class Movie extends Sequelize.Model {}
+Movie.init({
+  title: Sequelize.STRING
+}, { sequelize }); // same as { sequelize: sequelize }
+
 // async IIFE
 (async () => {
+  // Sync 'Movies' Table
+  await sequelize.sync({ force: true });
+
   try {
-    await sequelize.authenticate();
-    console.log('Connection to the database successful!');
+    // Instance of the Movie class represents a database row
+    const movie = await Movie.create({
+      title: 'Toy Story',
+    });
+    console.log(movie.toJSON());
+
+    // New Entry
+    const movie2 = await Movie.create({
+      title: "War Dogs"
+    });
+    console.log(movie2.toJSON());
+
   } catch (error) {
     console.error('Error connecting to the database: ', error);
   }
