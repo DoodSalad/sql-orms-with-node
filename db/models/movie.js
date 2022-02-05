da0e4745461581328f096c87539ef1e369929219
Riley Hays <haysrg@miamioh.edu>
2022-02-05 07:36:17 -0500

    Added validation errors to the Movie.js file checking for and throwing an error when the user inputs an invald data type when creating a new movie.

--- db/models/movie.js
+++ db/models/movie.js
@@ -12,14 +12,39 @@ module.exports = (sequelize) => {
     title: {
       type: Sequelize.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for "title"',
+        },
+        notEmpty: {
+          //Custom error message
+          msg: 'Please provide a value for "title"' ,
+        },
+       },
     },
     runtime: {
       type: Sequelize.INTEGER,
       allowNull: false,
+      validate {
+        min: {
+          args: 1,
+          msg: 'Please provide a value greater than "0" for "runtime"',
+        },
+        notNull : {
+          msg: 'Please provide a value for "runtime"',
+        },
+       },
      },
     releaseDate: {
       type: Sequelize.DATEONLY,
       allowNull: false,
+      validate {
+        notNull: 'Please provide a value for "releaseDate"',
+       },
+       isAfter: {
+         args: '1895-12-27',
+         msg: 'Please provide a value on of after "1895-12-28" for "releaseDate"',
+       },
      },
     isAvailableOnVHS: {
       type: Sequelize.BOOLEAN,
